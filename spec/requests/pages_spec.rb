a9f254dae73190d7ef74725259d1ff728c2aee84
Nagaraja Muppavarapu <nmuppavarapu@gmail.com>
2013-08-24 08:54:39 -0500

    Finish static pages

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,7 +8,11 @@ describe "Pages" do
          visit '/pages/home'
          page.should have_content("Sample App")
       end
-   
+      
+      it "should have page title 'Home'" do
+         visit '/pages/home'
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Demo App | Home")
+      end
    end
    
    describe "Help Page" do
@@ -17,23 +21,38 @@ describe "Pages" do
          visit '/pages/help'
          page.should have_content("Help Page")
       end
+      
+      it "should have page title 'Help'" do
+         visit '/pages/help'
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Demo App | Help")
+      end
    
    end
    
    describe "About Page" do
       
-      it "should have the content 'About Page'" do
+      it "should have the content 'About Us'" do
          visit '/pages/about'
-         page.should have_content("About Page")
+         page.should have_content("About Us")
+      end
+      
+      it "should have page title 'About Us'" do
+         visit '/pages/about'
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Demo App | About Us")
       end
    
    end
    
    describe "Contact Page" do
       
-      it "should have the content 'Contact Page'" do
+      it "should have the content 'Contact'" do
          visit '/pages/contact'
-         page.should have_content("Contact Page")
+         page.should have_content("Contact")
+      end
+      
+      it "should have page title 'Contact'" do
+         visit '/pages/contact'
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Demo App | Contact")
       end
    
    end   
